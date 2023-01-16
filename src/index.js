371ba36c566e35efbc8c89cd9d854f32108b43fd
Evgeni <mg4rulez@gmail.com>
2023-01-16 21:15:41 +0300

    slove the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function toReadable (number) {
-  
+    const numbers = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten' ];
+    const secondDecade = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen' ];
+    const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+
+    let numberArr = number.toString().split('');
+
+    function getPrimeNumber() {
+        let lastNumber = numberArr.length - 1;
+            return numbers[numberArr[lastNumber]];
+    };
+
+    function getDoubleNumber() {
+        let lastTwoNumbersArr = numberArr.slice(-2);
+        let condition = +lastTwoNumbersArr.join('');
+            if ( condition > 9 && condition < 20 ) {
+                return secondDecade[lastTwoNumbersArr[1]];
+            } else {
+                return +lastTwoNumbersArr[1] === 0 ? tens[lastTwoNumbersArr[0] - 2] : `${tens[lastTwoNumbersArr[0] - 2]} ${numbers[lastTwoNumbersArr[1]]}`;
+            }
+    }
+    
+    function getTripleNumber () {
+        if (number % 100 === 0) {
+            return `${numbers[numberArr[0]]} hundred`
+        } else if (+numberArr[1] === 0) {
+            return `${numbers[numberArr[0]]} hundred ${getPrimeNumber()}`
+        } else {
+            return `${numbers[numberArr[0]]} hundred ${getDoubleNumber()}`
+        }
+    }
+
+    switch(numberArr.length) {
+        case 1:
+            return getPrimeNumber();
+        case 2:
+            return getDoubleNumber();
+        case 3:
+            return getTripleNumber();
+    }
 }
+
